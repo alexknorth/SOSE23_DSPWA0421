6a4699d0aab2923037126dbae8532185a16dcc50
Alexander Krüger <alexander.krueger@northcodes.de>
2023-06-16 14:57:22 +0200

    removed buttons for testing api calls

---
+++ Data/websocketserver.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const ws = require('ws');
+
+const app = express();
+
+// Set up a headless websocket server that prints any
+// events that come in.
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', (socket) => {
+  // Log a message when a new client connects
+  console.log('client connected.');
+  // Listen for incoming WebSocket messages
+  socket.on('message', (data) => {
+
+   // Broadcast the message to all connected clients
+   wsServer.clients.forEach(function each(client) {
+      if (client !== socket && client.readyState === WebSocket.OPEN) {
+        client.send(data.toString());
+        // console.log("message",data.toString())
+      }
+    });
+  });
+  // Listen for WebSocket connection close events
+  socket.on('close', () => {
+    // Log a message when a client disconnects
+    console.log('Client disconnected');
+  });
+});
+
+// `server` is a vanilla Node.js HTTP server, so use
+// the same ws upgrade process described here:
+// https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
+const server = app.listen(3001);
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, socket => {
+    wsServer.emit('connection', socket, request);
+  });
+});
--- src/components/WeatherDetails.vue
+++ src/components/WeatherDetails.vue
@@ -80,13 +80,8 @@
       </v-card-actions>
     </v-card>
   </div>
- <!-- <div>
-    <v-btn @click.prevent="getGeoData('Hamburg')"> GeoData </v-btn>
-    <v-btn @click="getPredictions('Hamburg', 3)">7 Tage </v-btn>
-    <v-btn @click.prevent="getCurrentWeather('Hamburg')"> Heute </v-btn>
-  </div>-->
 </template>
-  
+
 <script>
 import WeatherData from "../dataProvider.js";
 
