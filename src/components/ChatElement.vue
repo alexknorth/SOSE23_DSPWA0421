cf29d4d3d3d866590ec39f4b10bdba3429fd3b20
Alexander Krüger <alexander.krueger@northcodes.de>
2023-06-16 17:18:11 +0200

    Chat fixes

--- src/components/ChatElement.vue
+++ src/components/ChatElement.vue
@@ -1,29 +1,23 @@
 <template>
-  <div>
-    <div v-if="!connect">
-      <div>
-        <div>
-          <form @submit.prevent="connectWebSocket">
-            <v-label>Enter your name to start chatting</v-label>
-            <input type="text" v-model="username" placeholder="Name" />
-            <v-divider></v-divider>
-            <button type="submit">Connect</button>
-          </form>
-        </div>
-      </div>
+  <v-card variant="outlined">
+    <div v-if="!connected">
+      <form @submit.prevent="connectWebSocket">
+        <v-label>Enter your name to start chatting</v-label>
+        <input type="text" v-model="username" placeholder="Name" />
+        <v-divider></v-divider>
+        <button type="submit">Connect</button>
+      </form>
     </div>
     <div v-else>
-      <div>
-        <div
-          v-for="(message, index) in messages"
-          :key="index"
-          :class="[
-            message.username === username ? 'left-bubble' : 'right-bubble',
-          ]"
-        >
-          <b>{{ message.username }}</b
-          >: <em>{{ message.text }}</em>
-        </div>
+      <div
+        v-for="(message, index) in messages"
+        :key="index"
+        :class="[
+          message.username === username ? 'left-bubble' : 'right-bubble',
+        ]"
+      >
+        <b>{{ message.username }}</b
+        >: <em>{{ message.text }}</em>
       </div>
       <div>
         <form @submit.prevent="handleMessageSubmit(username, text)">
@@ -32,7 +26,7 @@
         </form>
       </div>
     </div>
-  </div>
+  </v-card>
 </template>
   
 <script>
@@ -45,29 +39,29 @@ export default {
       messages: [],
       text: "",
       socket: null,
-      connect: false,
+      connected: false,
       connection: null,
     };
   },
   methods: {
     connectWebSocket() {
-      console.log("Starting connection to WebSocket Server");
+      console.log("Starting connection to WebSocket Server...");
       this.connection = new WebSocket("ws://localhost:3001");
 
-      this.connection.onmessage = function (event) {
-        //console.log(event);
-        this.messages.push(event.data);
+      this.connection.onmessage = (event) => {
+        this.messages.push(JSON.parse(event.data));
       };
 
-      this.connection.onopen = function () {
-        this.connect = true;
-        //console.log(event)
+      this.connection.onopen = () => {
+        this.connected = true;
         console.log("Successfully connected to the echo websocket server...");
       };
     },
-    handleMessageSubmit(username, text) {
-      console.log(this.connection);
-      this.connection.send(JSON.stringify({ username: username, text: text }));
+    handleMessageSubmit(username) {
+      const newMessage = { username: username, text: this.text };
+      this.connection.send(JSON.stringify(newMessage));
+      this.messages.push(newMessage);
+      this.text = '';
     },
   },
   mounted: function () {},
@@ -76,4 +70,44 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.left-bubble {
+    position: relative;
+  display: inline-block;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #f0f0f0;
+  color: #333;
+}
+
+.left-bubble::before {
+    content: '';
+  top: 50%;
+  left: -20px;
+  transform: translateY(-50%);
+  width: 20px;
+  height: 20px;
+  background-color: #f0f0f0;
+  border-top-right-radius: 50%;
+}
+
+.right-bubble {
+  position: relative;
+  display: inline-block;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #f0f0f0;
+  color: #333;
+  text-align: right;
+}
+
+.right-bubble::before {
+  content: '';
+  top: 50%;
+  right: -20px;
+  transform: translateY(-50%);
+  width: 20px;
+  height: 20px;
+  background-color: #f0f0f0;
+  border-top-left-radius: 50%;
+}
 </style>
